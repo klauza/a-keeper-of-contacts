5f32d9f7f2a613231d363a2fc9f61e0a98963c77
klauza <klauza.uk@gmail.com>
2019-07-11 19:44:07 +0100

    display filtered contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -9,11 +9,22 @@ const Contacts = () => {
   const contactContext = useContext(ContactContext); 
   // now there is an access to any state/methods associated to this context
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if(contacts.length === 0){
+    return <h4>Please add a contact</h4>
+  }
 
   return (
     <Fragment>
-      {contacts.map(contact => ( <ContactItem key={contact.id} contact={contact} />) )}
+      {/* check if there is anything in 'filtered' state */}
+      {filtered !== null ? 
+        filtered.map(contact => (<ContactItem key={contact.id} contact={contact} />)) 
+        :
+        contacts.map(contact => (<ContactItem key={contact.id} contact={contact} />))
+      }
+
+     
     </Fragment>
   )
 }
