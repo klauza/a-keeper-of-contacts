5b0e1169912c52559f3221310458e20115f64c98
klauza <klauza.uk@gmail.com>
2019-07-16 14:41:21 +0100

    display contacts when user logs in, add spinner

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,15 +1,21 @@
 // pull contacts from a state in here and then looping through them (map) and output in ContactItem for each one
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
 import ContactContext from '../../context/contact/contactContext';
 
 import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
 
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext); 
   // now there is an access to any state/methods associated to this context
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    //eslint-disable-next-line
+  }, []); 
 
   if(contacts.length === 0){
     return <h4>Please add a contact</h4>
@@ -17,17 +23,24 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {/* check if there is anything in 'filtered' state */}
-      {filtered !== null ? 
-        filtered.map(contact => (<ContactItem key={contact._id} contact={contact} />)) 
-        :
-        contacts.map(contact => (<ContactItem key={contact._id} contact={contact} />))
+
+      {contacts !== null && !loading ? 
+        (
+          filtered !== null ? 
+          filtered.map(contact => (<ContactItem key={contact._id} contact={contact} />)) 
+          :
+          contacts.map(contact => (<ContactItem key={contact._id} contact={contact} />))
+        
+        ) 
+
+        : 
+        
+        <Spinner /> 
       }
 
-     
     </Fragment>
-  )
-}
+  );
+};
 
 
 export default Contacts
